526f324d0d22d99fd1dd99740b5657938030c364
Drew <80690341+drewgillen@users.noreply.github.com>
2022-01-28 21:59:33 -0500

    Delete .history directory

--- .history/setup_20220128204501.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='Abyiss',
-      version='0.1',
-      description='The Abyiss Client Library',
-      author_email='support@abyiss.com',
-      url='abyiss.com',
-      packages['AbyissClient']
-      )
--- .history/setup_20220128214509.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='Abyiss',
-      version='0.7',
-      description='The Abyiss Client Library',
-      author_email='support@abyiss.com',
-      url='https://github.com/Abyiss/Client-python',
-      packages=['AbyissClient'],
-      )
