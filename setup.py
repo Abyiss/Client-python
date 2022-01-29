fc130fc99398237ede4e2712efd0ab5448c23449
Drew <80690341+drewgillen@users.noreply.github.com>
2022-01-28 22:00:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(name='Abyiss',
-<<<<<<< HEAD
       version='0.9',
       description='The Abyiss Client Library',
       author_email='support@abyiss.com',
@@ -9,11 +8,3 @@ setup(name='Abyiss',
       packages=['AbyissClient'],
       install_requires=['requests']
       )
-=======
-      version='0.1',
-      description='The Abyiss Client Library',
-      author_email='support@abyiss.com'
-      url='abyiss.com'
-      packages['AbyissClient']
-      )
->>>>>>> 33555758864d8dcb4ffbbe79237c132d2ab404fe
