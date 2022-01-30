c0d0e8becd326045487be05c5d026790ebe5758e
Drew Gillen <dgillenbusiness@gmail.com>
2022-01-29 20:54:09 -0500

    added stuff to file

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
 from setuptools import setup
 
 setup(name='Abyiss',
-<<<<<<< HEAD
-      version='0.9',
+      version='0.11',
       description='The Abyiss Client Library',
       author_email='support@abyiss.com',
       url='https://github.com/Abyiss/Client-python',
       packages=['AbyissClient'],
       install_requires=['requests']
       )
-=======
-      version='0.1',
-      description='The Abyiss Client Library',
-      author_email='support@abyiss.com'
-      url='abyiss.com'
-      packages['AbyissClient']
-      )
->>>>>>> 33555758864d8dcb4ffbbe79237c132d2ab404fe
