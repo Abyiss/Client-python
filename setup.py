ca1053be1aebb306838c819e8f8b33dc96edf254
theodore ikehara <tikehara@ucsc.edu>
2022-02-03 12:08:54 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
       name='Abyiss',
-      version='0.0.3',
+      version='0.0.4',
       description='The Abyiss Client Library',
       author_email='support@abyiss.com',
       url='https://github.com/Abyiss/Client-python',
@@ -14,4 +14,34 @@ setup(
             'Abyiss': 'https://abyiss.com/',
             'Github': 'https://github.com/Abyiss/Client-python'
       },
+
+    classifiers = {
+        'Development Status :: 6 - Mature',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Office/Business :: Financial :: Point-Of-Sale',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Life',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+        'Topic :: Database',
+        'Framework :: Datasette',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Database :: Database Engines/Servers',
+        'Natural Language :: English',
+        },
+
+
 )
